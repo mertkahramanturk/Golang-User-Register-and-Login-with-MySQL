b054c436f32e76c0382a744f8303d5a9bb01a007
Mert Kahramantürk <kahramanturkk@gmail.com>
2022-05-07 11:06:19 +0300

    first commit

---
+++ config/database-config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"UserLogin2/entity"
+	"fmt"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+//SetupDataBaseConnection is creating a new connection to our database
+func SetupDatabaseConnection() *gorm.DB {
+
+	ErrEnv := godotenv.Load()
+
+	if ErrEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	/*
+		dbUser := os.Getenv("DB_USER")
+		dbPass := os.Getenv("DB_PASS")
+		dbHost := os.Getenv("DB_HOST")
+		dbName := os.Getenv("DB_NAME")
+	*/
+
+	dsn := fmt.Sprintf("root:159357Mert*@tcp(127.0.0.1:3306)/mysql?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to create a connection to your database")
+	}
+
+	db.AutoMigrate(&entity.Book{}, &entity.User{})
+
+	return db
+}
+
+//CloseDatabseConnection method is closing a connection between your app and your db
+
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+
+	if err != nil {
+		panic("Failed to close connection from database")
+	}
+	dbSQL.Close()
+}
---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"UserLogin2/config"
+	"UserLogin2/controller"
+	"UserLogin2/repository"
+	"UserLogin2/service"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = config.SetupDatabaseConnection()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	jwtService     service.JWTService        = service.NewJWTService()
+	authService    service.AuthService       = service.NewAuthService(userRepository)
+	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+)
+
+func main() {
+	defer config.CloseDatabaseConnection(db)
+	r := gin.Default()
+
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
+
+	r.Run()
+}
